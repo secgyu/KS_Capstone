c7a891cbafe1ac8dcd3e9b7d304501e0ab9a5fd4
김규민 <abc2232002@gmail.com>
2025-04-27 12:17:58 +0900

    fix: add NSAppTransportSecurity

--- front/app.config.js
+++ front/app.config.js
@@ -20,6 +20,15 @@ export default {
         NSPhotoLibraryAddUsageDescription: "앱에서 사진을 추가할 수 있도록 허용합니다.",
         NSUserNotificationUsageDescription: "앱에서 알림을 보내기 위해 권한이 필요합니다.",
         NSAppleMusicUsageDescription: "앱 내 오디오 재생 및 음원 라이브러리 접근을 위해 필요합니다.",
+        NSAppTransportSecurity: {
+          NSAllowsArbitraryLoads: true,
+          NSExceptionDomains: {
+            "15.165.132.138:3030": {
+              NSIncludesSubdomains: true,
+              NSTemporaryExceptionAllowsInsecureHTTPLoads: true,
+            },
+          },
+        },
       },
     },
     android: {
